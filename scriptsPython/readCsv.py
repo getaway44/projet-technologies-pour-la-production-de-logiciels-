f520d9df68ccfe07108a2456b3ffd89c3a9e3170
Benjamin SECHE <E145725X@U-INF-J-F113-16.iut-nantes.univ-nantes.prive>
2016-02-04 10:02:24 +0100

    modif script de lecture des tables

--- scriptsPython/readCsv.py
+++ scriptsPython/readCsv.py
@@ -1,15 +1,19 @@
 import csv
-def read(path):
-	fichier = csv.reader(open(path, "rt"))
-
-	if(path == "/hometu/etudiants/c/h/E145530K/Documents/Info2/Tech Prod Logiciel/projet-technologies-pour-la-production-de-logiciels-/csv/installations.csv"):
+def read(nomFichier):
+	if(nomFichier == "installations"):
+		fichier = csv.reader(open("/hometu/etudiants/s/e/E145725X/semestre4/python/projet-technologies-pour-la-production-de-logiciels-/csv/installations.csv", "rt"), delimiter=',')
 		for row in fichier:
 			print(row[0], row[1], row[2], row[3], row[7], row[9], row[10])
-
-	elif(path == "/hometu/etudiants/c/h/E145530K/Documents/Info2/Tech Prod Logiciel/projet-technologies-pour-la-production-de-logiciels-/csv/equipements.csv"):
+	
+	elif(nomFichier == "equipements"):
+		fichier = csv.reader(open("/hometu/etudiants/s/e/E145725X/semestre4/python/projet-technologies-pour-la-production-de-logiciels-/csv/equipements.csv", "rt"), delimiter=',')
 		for row in fichier:
 			print(row[3], row[4])
-
-	elif(path == "/hometu/etudiants/c/h/E145530K/Documents/Info2/Tech Prod Logiciel/projet-technologies-pour-la-production-de-logiciels-/csv/activites.csv"):
+		
+	elif(nomFichier == "activites"):
+		fichier = csv.reader(open("/hometu/etudiants/s/e/E145725X/semestre4/python/projet-technologies-pour-la-production-de-logiciels-/csv/activites.csv", "rt"), delimiter=',')
 		for row in fichier:
 			print(row[4], row[5])
+
+if __name__ == '__main__':
+	read(input())
