0b933f19656c511dd5a54160c98056328633a51a
Benjamin SECHE <E145725X@U-INF-J-F113-23.iut-nantes.univ-nantes.prive>
2016-02-25 08:58:10 +0100

    refactoring

---
+++ scriptsPython/equipement/equipementImporter.py
@@ -0,0 +1,12 @@
+import csv
+def read(path):
+	fichier = csv.reader(open(path, "rt"), delimiter=',')
+
+	for row in fichier:
+		print(row[2], row[3], row[4]) # Affiche les colonnes : "InsNumeroInstall", "InsNom" et "EquipementId".
+
+if __name__ == '__main__':
+    read("/hometu/etudiants/c/h/E145530K/Documents/Info2/Tech Prod Logiciel/projet-technologies-pour-la-production-de-logiciels-/csv/equipements.csv")
+    
+
+    Status API Training Shop Blog About Pricing 
